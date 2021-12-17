914f52c2b7d58a1234a81a4f2190117d8353ac3a
Yasar Arafat <ysulaiman@msystechnologies.com>
2021-12-17 16:44:02 +0530

    fail

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn React
+          learn some React
         </a>
         <p> Sample React Application hellow world </p>
       </header>
